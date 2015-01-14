9e27b92a9216eff26d3696b72b8b5794988f7f62
kickooyoo <mtle@umich.edu>
2015-01-13 20:13:40 -0500

    added shiny new version of struct_find_field that can search by value, with tolerance, and exhaustive results
    elaborated in comments

---
+++ mai_struct_find_field.m
@@ -0,0 +1,121 @@
+  function [out_name, out_val] = mai_struct_find_field(ss, varargin)
+%|function [out_name, out_val] = ir_struct_find_field(ss, varargin)
+%|
+%| Look recursively through the fields of a structure for a given field.
+%| Objects like Fatrix and fatrix2 are converted to structs too for this.
+%|
+%| Returns ss.(field) or ss.?.(field) or ss.?.?.(field) etc.
+%|  
+%| Edited by Mai 01/13/2015 for varargin options:
+%| - can search by value
+%| - can search by value within tolerance 
+%| - exhaustive option returns all fields that match query, not just first
+%| varargin:
+%|		fieldname
+%|		fieldval
+%|		all_vector		default: 1
+%|			requires element-by-element match w/i tolerance for each element
+%|			if off, returns field if any vector element matches
+%|			for use with fieldval
+%|		fieldtol		default: 0
+%|			for use with fieldval
+%| 		exhaustive 		default: 0
+
+arg.fieldname = [];
+arg.fieldval = [];
+arg.fieldtol = 0;
+arg.all_vector = 1;
+arg.exhaustive = 0;
+arg = vararg_pair(arg, varargin);
+
+if nargin == 1 && streq(ss, 'test'), ir_struct_find_field_test, return, end
+if nargin < 3, help(mfilename), error(mfilename), end
+
+[out_name, out_val, list_name, list_val, ok_name, ok_val] = ir_struct_find_field_do(ss, arg, {}, {});
+if ~ok_name && ~ok_val
+	fail_msg = 'could not find field';
+	if ~isempty(arg.fieldval)
+		fail_msg = sprintf('%s with value %d', fail_msg, arg.fieldval);
+	end
+	if ~isempty(arg.fieldtol)
+		fail_msg = sprintf('%s with tolerance %d', fail_msg, arg.fieldtol);
+	end
+	if ~isempty(arg.fieldname)
+		fail_msg = sprintf('%s with name %s', fail_msg, arg.fieldname);
+	end
+	fail([fail_msg '"%s" in "%s"'], fail_msg, inputname(1));
+end
+if arg.exhaustive
+	out_name = list_name;
+	out_val = list_val;
+end
+
+
+function [out_name, out_val, list_name, list_val, ok_name, ok_val] = ir_struct_find_field_do(ss, arg, list_name, list_val)
+
+out_name = [];
+out_val = [];
+ok_name = isempty(arg.fieldname);
+ok_val = isempty(arg.fieldval);
+fnames = fieldnames(ss);
+for ii=1:length(fnames)
+	fname = fnames{ii};
+
+	s1 = ss.(fname);
+	if isa(s1, 'Fatrix') || isa(s1, 'fatrix2')
+		s1 = struct(s1); % trick
+	end
+	
+	if ~ok_val && ~isempty(s1) && isa(s1, 'numeric')
+		out_name = fname;
+		out_val = s1;	
+		within_tol = abs(arg.fieldval(:) - s1(:))/max(col(arg.fieldval)) <= arg.fieldtol;
+		if arg.all_vector
+			ok_val = all(within_tol);
+		else
+			ok_val = any(within_tol);
+		end
+	end
+
+	if ~ok_name && ~isempty(strfind(lower(fname), lower(arg.fieldname)))
+		out_name = fname;
+		out_val = s1;
+		ok_name = true;
+	end
+
+	if ~arg.exhaustive && ok_val && ok_name
+		return;
+	elseif ok_val && ok_name
+		list_name = [list_name; out_name];
+		list_val = [list_val; {out_val}];
+		out_name = [];
+		out_val = [];
+		ok_name = isempty(arg.fieldname);
+		ok_val = isempty(arg.fieldval);
+	end
+
+	if isstruct(s1) || isa(s1, 'twix_map_obj')
+		[out_name, out_val, new_list_name, list_val, ok_name, ok_val] = ir_struct_find_field_do(s1, arg, list_name, list_val);
+		for ii = length(list_name)+1:length(new_list_name)
+			new_list_name{ii} = sprintf('%s.%s', fname, new_list_name{ii});
+		end
+		list_name = new_list_name;
+		if ok_name && ok_val
+			return
+		end
+	end
+end
+
+
+% ir_struct_find_field_test()
+function ir_struct_find_field_test
+ss.a = 1;
+ss.b.c = 2;
+ss.b.d = 3;
+jf_equal(1, ir_struct_find_field(ss, 'a'))
+jf_equal(3, ir_struct_find_field(ss, 'd'))
+try
+	ir_struct_find_field(ss, 'bad')
+	fail 'should not get here'
+catch
+end
