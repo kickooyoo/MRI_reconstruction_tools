d6422fd8cdcad3cfd47ab73f34775667cbda9acb
kickooyoo <kickooyoo@gmail.com>
2015-04-09 15:36:37 -0400

    set global max across frames, explicitly deal with complex images

--- make_gif.m
+++ make_gif.m
@@ -1,10 +1,17 @@
 function make_gif(x, filename)
 %function make_gif(x, filename)
 
+if ~isreal(x)
+	x = abs(x);
+	display('warning: taking absolute value of complex x');
+end
+
 if ~exist([filename '.gif'])
 	figure; 
+	max_val = max(x);
+	min_val = min(x);
 		    for ff = 1:size(x,3)
-   			im(x(:,:,ff));
+   			im(x(:,:,ff), [min_val max_val]);
 			drawnow;
 			frame = getframe();
 			img = frame2im(frame);
