65f3a1757ac96ed931deddfed03b837b3104ad6a
kickooyoo <kickooyoo@gmail.com>
2015-07-09 19:38:05 -0400

    radial datasharing improvements - decomposed dcf - padded ring simplified in dcf - max radius flag now accounts for gradient delay - better handling for case where not enough spokes to reach Nyquist

---
+++ calculate_voronoi_dcf.m
@@ -0,0 +1,77 @@
+function dcf = calculate_voronoi_dcf(freqs, delta_ro, arg)
+%function dcf = calculate_voronoi_dcf(freqs, delta_ro, arg)
+%
+% gives voronoi area for a set of frequency points, allows for duplicate
+% points (e.g. at DC)
+%
+%
+arg.pad_ring = true;
+
+% pare down all frequencies into set of unique points
+% cannot use Matlab's unique because need to reexpand at end
+for ii = 1:length(freqs)
+	all_instances = find(freqs == freqs(ii));
+	if length(all_instances) < 1, keyboard; end
+	if max(all_instances) > length(freqs), keyboard; end
+	first_instance(ii) = all_instances(1);
+end
+uniq_freqs = freqs(find(first_instance == (1:length(freqs))));
+Nuniq = length(uniq_freqs);
+
+if arg.pad_ring
+	max_radius = max(abs(col(uniq_freqs)));
+	Npad = 2*sum(abs(abs(freqs) - max_radius) < delta_ro*1e-4);
+	ring = (max_radius + delta_ro)*exp(-1*i*linspace(0,2*pi,Npad)');
+	uniq_freqs = [uniq_freqs; ring];
+end
+
+% use Matlab's built in Voronoi commands to find area of nearest neighbor
+% cells
+if arg.figs_on
+	try
+		figure;
+		voronoi(real(uniq_freqs), imag(uniq_freqs))
+		axis equal;
+		axis tight;
+	catch
+		display('error when attempting to plot voronoi cells');
+		keyboard;
+	end
+end
+[vor_v, vor_c] = voronoin([real(uniq_freqs) imag(uniq_freqs)]);
+
+% for each centroid, calculate area from vertices of polygon
+% only go up to Nuniq, remainder points are for padding ring
+for jj = 1:Nuniq 
+	A(jj) = polyarea(vor_v(vor_c{jj},1), vor_v(vor_c{jj},2));
+	tmpx = vor_v(vor_c{jj},1);
+	tmpy = vor_v(vor_c{jj},2);
+	out_of_bounds = any(dist([tmpx'; tmpy'], zeros(2, size(tmpx,1))) > ...
+		arg.max_radius + arg.pad_ring*delta_ro);
+	if isnan(A(jj)) || out_of_bounds
+		display('invalid polygon');
+		keyboard;
+	end
+	if (mod(jj,100) == 0), display(sprintf('done with Voronoi areas for %d/%d', jj, Nuniq)); end
+end
+uniq_dcf = col(A);
+
+uniq_spread_dcf = embed(uniq_dcf, first_instance == (1:length(freqs)));
+for ii = 1:length(freqs)
+	if first_instance(ii) > length(uniq_spread_dcf), keyboard; end
+	num_occurences = length(find(first_instance == first_instance(ii)));
+	dcf(ii) = uniq_spread_dcf(first_instance(ii))/num_occurences;
+end
+dcf = col(dcf);
+
+if ~all(size(dcf) == size(freqs))
+	display('size mismatch with dcf and freqs');
+	keyboard;
+end
+end
+
+% Euclidean distance
+function dist = dist(x,y)
+% good for complex vals
+dist = sqrt(sum(abs(x - y).^2));
+end
