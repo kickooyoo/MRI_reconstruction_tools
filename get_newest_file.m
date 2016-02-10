a5b3f08373edee1a20623fb2cef1665927b313f2
kickooyoo <kickooyoo@gmail.com>
2016-02-10 14:02:56 -0500

    gets newest version of file matching regexp

---
+++ get_newest_file.m
@@ -0,0 +1,24 @@
+function fname = get_newest_file(regexp)
+% function fname = get_newest(regexp)
+% returns filename of the newest file fitting the input regular expression
+%
+% to do: parse off directories, allow file names to have paths
+
+listing = dir('.');
+fname = [];
+fdate = 0;
+for ii = 1:length(listing)
+        if listing(ii).isdir
+                break
+        end
+        curr_name = listing(ii).name;
+        exp_match = regexpi(fname, regexp);
+        if isempty(exp_match)
+                break
+        end
+        curr_date = listing(ii).datenum;
+        if curr_date > fdate
+                fname = curr_name;
+                fdate = curr_date;
+        end
+end:w
