f61e3c2b188dd459062b189a5455074badf553b5
kickooyoo <kickooyoo@gmail.com>
2016-04-19 16:20:53 -0400

    saves .eps and .fig of current figure

---
+++ save_im.m
@@ -0,0 +1,13 @@
+function save_im(path, name, varargin)
+% function save_im(path, name, varargin)
+arg.FontSize = [];
+arg = vararg_pair(arg, varargin);
+
+% if ~isempty(arg.FontSize)
+	
+
+print([path name], '-depsc');
+savefig([path name])
+display(sprintf('saved .fig and .eps named %s in %s', name, path))
+
+
